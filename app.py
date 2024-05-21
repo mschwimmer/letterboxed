1f3d3b25e29dcad37c6675ba42fade1289516625
mschwimmer <mschwimmer1234@gmail.com>
2024-05-21 14:44:31 -0400

    Created separate route for check letter

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ BOARD = Board(TODAY_METADATA['sides'])
 SOLVER = LetterBoxedSolver(BOARD, "words_easy.txt")
 TWO_WORD_SOLUTIONS = SOLVER.get_two_word_solutions()
 FLAT_TWO_WORD_SOLUTIONS = {word for solution in TWO_WORD_SOLUTIONS for word in solution}
+
+
 # Create a dict that matches first letter to list of words with first letter
 # FIRST_LETTER_MAP = {first_letter: [word for word in FLAT_TWO_WORD_SOLUTIONS if word.startswith(first_letter)] for first_letter in set(word[0] for word in FLAT_TWO_WORD_SOLUTIONS)}
 
 
 @app.route('/')
 def index():
-
     return render_template('index.html',
                            sides=TODAY_METADATA['sides'],
                            nyt_solution=TODAY_METADATA['nyt_solution'],
@@ -47,22 +48,22 @@ def check_word():
         print(FLAT_TWO_WORD_SOLUTIONS)
         print(user_input in FLAT_TWO_WORD_SOLUTIONS)
         return render_template('index.html',
-                            sides=TODAY_METADATA['sides'],
-                            nyt_solution=TODAY_METADATA['nyt_solution'],
-                            prog_solutions=TWO_WORD_SOLUTIONS,
-                            is_correct=is_correct,
-                            user_word=user_input)
+                               sides=TODAY_METADATA['sides'],
+                               nyt_solution=TODAY_METADATA['nyt_solution'],
+                               prog_solutions=TWO_WORD_SOLUTIONS,
+                               is_correct=is_correct,
+                               user_word=user_input)
 
 
 @app.route('/check_letter', methods=['POST'])
 def check_letter():
     if request.method == 'POST':
         user_input = request.form['user_letter'].lower()
-        valid_words = SOLVER.get_words_from_first_letter_dict().get(user_input)
+        valid_words = SOLVER.get_words_from_first_letter_dict().get(user_input, None)
         print(user_input)
         print(valid_words)
 
-        return render_template('index.html',
+        return render_template('check-letter.html',
                                sides=TODAY_METADATA['sides'],
                                nyt_solution=TODAY_METADATA['nyt_solution'],
                                prog_solutions=TWO_WORD_SOLUTIONS,
